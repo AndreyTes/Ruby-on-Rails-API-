f7264e195cd584b21a795666b570ba3f3603a716
AndreyTes <andrey_tes2@mail.ru>
2021-07-01 15:10:13 +0300

    initial_commit

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  validates :email, presence: true, uniqueness: true
-  validates :password, length: { in: 6..20 }
-  has_many :posts, dependent: :destroy
-  has_many :comments
-end
